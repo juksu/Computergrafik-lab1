3823375a995bfe6287520263443f1aa171915194
Kettu <fuchs.shcuf@gmail.com>
2015-10-12 20:44:44 +0200

    spinning square

--- lab1.js
+++ lab1.js
@@ -1,17 +1,36 @@
 "use strict";
 
+
+var DIMENSIONS = 2;
+var gl;
+var vertices = [];
+var modelViewMatrixLoc;
+var modelViewMatrix;
+
+
 function webGLstart()
 {
-	var DIMENSIONS = 2;
-	var canvas;
-	var vertices = [];
-	var gl;
-	
-	canvas = document.getElementById( "glCanvas" );
+
+	var canvas = document.getElementById( "glCanvas" );
 	
 	gl = WebGLUtils.setupWebGL( canvas );
 	if ( !gl )
 		alert( "WebGL is not available" );
+	
+	/**
+	 * Configure Webgl
+	 */
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+
+	/**
+	 * Load shaders and initialize attribute buffers
+	 */
+	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );	
 		
 	/**
 	 * construct tetris elements with triangles
@@ -74,38 +93,63 @@ function webGLstart()
 //	for( var i = 0; i < lBlock.length; i++ )
 //		vertices.push(lBlock[i]);
 	
+
 	/**
-	 * Configure Webgl
+	 * Load the data into the GPU
 	 */
-	gl.viewport( 0, 0, canvas.width, canvas.height );
-	gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW );
+
 	
 	/**
-	 * Load shaders and initialize attribute buffers
+	 * Model view matrix
 	 */
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+//	mat4.identity(mvMatrix);
+//	mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
 
-    /**
-     * Load the data into the GPU
-     */
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW );
-    
-    
+
 	/**
 	 * Associate out shader variables with our data buffer
 	 */
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, DIMENSIONS, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    
-    render(gl, vertices, square.length);
+	var vPosition = gl.getAttribLocation( program, "vPosition" );
+	gl.vertexAttribPointer( vPosition, DIMENSIONS, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition );
+
+
+
+	modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+
+	/**
+	 * modelView transformations (scale, and translate)
+	 */
+	modelViewMatrix = mat4.create();	
+	mat4.scale(modelViewMatrix, mat4.create(), vec3.fromValues(0.1,0.1,1));
+	mat4.translate(modelViewMatrix, modelViewMatrix, vec3.fromValues(-4,0,0));
+	
+	render();
 }
 
-function render(gl, vertices, verticesLength)
+function render()
 {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, verticesLength );
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	
+
+	/**
+	 * modelView transformations (rotate around z axis) 
+	 * -> the transformation here causes a constant spinning
+	 */
+	mat4.rotateZ(modelViewMatrix, modelViewMatrix, 0.01);
+	
+	gl.uniformMatrix4fv(modelViewMatrixLoc, false, new Float32Array(modelViewMatrix));
+	
+//	gl.drawArrays( gl.TRIANGLES, 0, verticesLength );
+	/// TODO: noch einen weg finden das die verticesLength hier übergeben wird ohne in funktion zu übergeben
+	gl.drawArrays( gl.TRIANGLES, 0, 6 );
+	
+	/**
+	 * request browser to display the rendering the next time it wants to refresh the display
+	 * and then call the render function recorsively
+	 */
+	requestAnimFrame(render);
 }
