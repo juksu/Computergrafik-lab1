2ca742f0868bfda8b40b4b361011c5585a8680ca
juksu <fuchs.shcuf@gmail.com>
2015-10-15 14:19:55 +0200

    working on smooth transition

--- lab1.js
+++ lab1.js
@@ -8,12 +8,19 @@ var aspectRatio;		// aspect ratio of canvas
 var gl;
 var modelViewMatrixLoc;
 
-var modelViewMatrix;	
+var modelViewMatrix;
+
+var rotate = false;
+var rotateLeft = false;
+var translate = false;	
 
 var scalar = 2/10;		// initial value, scalar is changed with size of playing field
-var theta = 0;			// rotation
+var theta = 0.1;			// rotation
+var deltaTheta = 0;
 var xTranslate = 0;		// move along x-axis
+var deltaXTranslate = 0;
 var yTranslate = 0;		// move along y-axis
+var deltaYTranslate = 0;
 
 var vPosition;
 var vertexBufferHolder = [];
@@ -94,18 +101,19 @@ function render()
 //    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
 
-	modelViewMatrix = mat4.create();	
-	mat4.scale(modelViewMatrix, mat4.create(), vec3.fromValues(scalar,scalar*aspectRatio,1));
-	mat4.translate(modelViewMatrix, modelViewMatrix, vec3.fromValues(xTranslate,yTranslate,0));
-	mat4.rotateZ(modelViewMatrix, modelViewMatrix, theta);
+//	modelViewMatrix = mat4.create();	
+//	mat4.scale(modelViewMatrix, mat4.create(), vec3.fromValues(scalar,scalar*aspectRatio,1));
+//	mat4.translate(modelViewMatrix, modelViewMatrix, vec3.fromValues(xTranslate,yTranslate,0));
+//	mat4.rotateZ(modelViewMatrix, modelViewMatrix, theta);
 	
 	/// TODO different approach, instead of calculating the modelViewMatrix from identity all the time, only calculate the change to the previous one
-	/// TODO color!
 	
-	tetrominoHolder[tetrominoHolder.length - 1].modelViewMatrix = modelViewMatrix;
+//	tetrominoHolder[tetrominoHolder.length - 1].modelViewMatrix = modelViewMatrix;
 
-	for( var i = 0; i < tetrominoHolder.length; i++ )
-	{
+	
+
+	for( var i = 0; i < tetrominoHolder.length - 1; i++ )
+	{		
 		gl.bindBuffer( gl.ARRAY_BUFFER, vertexBufferHolder[i] );
 		gl.vertexAttribPointer( vPosition, DIMENSIONS, gl.FLOAT, false, 0, 0 );
 		
@@ -116,6 +124,48 @@ function render()
 		
 		gl.drawArrays( gl.TRIANGLES, 0, tetrominoHolder[i].vertices.length/DIMENSIONS );
 	}
+	
+	
+	// do the last tetromino
+	if( rotateLeft == true )
+	{
+		deltaTheta += 0.01;
+		var tempMV = [];
+		if( deltaTheta <= theta )
+		{
+			gl.uniformMatrix4fv(modelViewMatrixLoc, false, 
+					new Float32Array(mat4.rotateZ(
+							tempMV, tetrominoHolder[i].modelViewMatrix, deltaTheta)));
+		}
+		// rotation ended, calculate new modelViewMatrix, use theta to avoid errors
+		else
+		{
+			mat4.rotateZ(tetrominoHolder[i].modelViewMatrix, tetrominoHolder[i].modelViewMatrix, theta);
+			rotate = false;
+			deltaTheta = 0;
+		}
+	}
+	
+	gl.bindBuffer( gl.ARRAY_BUFFER, vertexBufferHolder[tetrominoHolder.length-1] );
+	gl.vertexAttribPointer( vPosition, DIMENSIONS, gl.FLOAT, false, 0, 0 );
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBufferHolder[tetrominoHolder.length-1] );
+	gl.vertexAttribPointer( vColor, COLORDIMENSIONS, gl.FLOAT, false, 0, 0 );
+	
+	gl.uniformMatrix4fv(modelViewMatrixLoc, false, new Float32Array(tetrominoHolder[tetrominoHolder.length-1].modelViewMatrix));
+	
+	gl.drawArrays( gl.TRIANGLES, 0, tetrominoHolder[tetrominoHolder.length-1].vertices.length/DIMENSIONS );	
+	
+		
+		
+		
+//	if( translate == true )
+	
+	
+	
+	
+	
+	
 
 /*
 	gl.bindBuffer( gl.ARRAY_BUFFER, vertexBufferHolder[vertexBufferHolder.length - 1] );
@@ -163,9 +213,11 @@ function controls()
 							yTranslate -= 1;
 							break;	
 					case 49: 	// 1
+							rotateLeft = true;
 							theta += Math.PI/2;
 							break;
 					case 51:	// 3
+							rotate = true;
 							theta -= Math.PI/2;
 							break;
 					case 13:	// enter
@@ -193,7 +245,7 @@ function addTetromino()
 	// a new tetromino is added. however only the newest tetromino is active (can be moved)
 	// calculate a final modelViewMatrix for the old tetromino so that it stays in its state and we save calculations
 	///TODO if scale, translate and rotate only always express change in it (see idea in render), this part is unnecessar 
-	if( tetrominoHolder.length != 0 )
+/*	if( tetrominoHolder.length != 0 )
 	{
 		modelViewMatrix = mat4.create();	
 		mat4.scale(modelViewMatrix, mat4.create(), vec3.fromValues(scalar,scalar*aspectRatio,1));
@@ -202,12 +254,37 @@ function addTetromino()
 		
 		tetrominoHolder[tetrominoHolder.length-1].modelViewMatrix = modelViewMatrix;
 	}
+*/	
 	
-	tetrominoHolder.push(new tetromino(spawnRandom(), mat4.create()));
+	// spawn at random position
+	var modelViewMatrix = [];
+	mat4.scale(modelViewMatrix, mat4.create(), vec3.fromValues(scalar,scalar*aspectRatio,1));
+//	mat4.translate(modelViewMatrix, modelViewMatrix, vec3.fromValues(xTranslate,yTranslate,0));
+//	mat4.rotateZ(modelViewMatrix, modelViewMatrix, theta);
+	
+	var xrand = Math.floor(Math.random()/scalar);
+	var yrand = Math.floor(Math.random()/scalar*aspectRatio);
+	
+	switch( Math.floor(Math.random()*4) )
+	{
+		case 1:	xrand *= -1;
+				break;
+		case 2:	yrand *= -1;
+				break;
+		case 3: xrand *= -1;
+				yrand *= -1;
+	}
+	
+	
+	mat4.translate(modelViewMatrix, modelViewMatrix, vec3.fromValues(xrand, yrand, 0));
+	
+	tetrominoHolder.push(new tetromino(spawnRandom(), modelViewMatrix));
 //	console.log("tetrominoHolder.length: " + tetrominoHolder.length );
 //	console.log("tetrominoHolder[" + (tetrominoHolder.length-1) + "].vertices.length: " + tetrominoHolder[tetrominoHolder.length-1].vertices.length );							
 //	console.log("tetrominoHolder[" + (tetrominoHolder.length-1) + "].color.length: " + tetrominoHolder[tetrominoHolder.length-1].color.length );							
 	
+	// spawn area
+		
 	/**
 	 * Load the data into the GPU
 	 */
