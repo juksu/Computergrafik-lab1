f242cd5660ab4f0b96df145bd26fc7d24b873ce9
Kettu <fuchs.shcuf@gmail.com>
2015-10-25 18:41:02 +0100

    reshaped tetrominos and created for each relative coordinates

--- Tetromino.js
+++ Tetromino.js
@@ -1,46 +1,19 @@
-/*
-function Tetromino( vertices, modelViewMatrix, relativeCollisionCoordinates )	///TODO local collision coordinates
-{	
-	this.vertices = vertices;
-	this.modelViewMatrix = modelViewMatrix;
-	this.relativeCollisionCoordinates = relativeCollisionCoordinates;
-	var col = [];
-	
-	// i simply want to have onecolored tetrominos
-	// -> assign each vertex the same color value
-	// for some fancy rainbowcolor effects assign different colors to the vertices but I was told rainbowcolor is bad ;)
-	// should not be to bright or dark -> use only random values between 50 and 200
-	var col1 = (Math.floor(Math.random()*150)+50)/256;
-	var col2 = (Math.floor(Math.random()*150)+50)/256;
-	var col3 = (Math.floor(Math.random()*150)+50)/256;
-		
-	for (var i=0; i < vertices.length/DIMENSIONS; i++)
-		col = col.concat( [col1, col2, col3, 1.0 ] );
-
-    this.color = col;
-    
-    
-    // relative coordinates of the collision
-}
-*/
-
-
-function Tetromino()
+var Tetromino = function()
 {	
 	/**
 	 * construct tetris elements with triangles
 	 * we want to have the vertices only on the corners of the block in order to have aligned objects
 	 * round the vector values pushed to vertices to achive that
 	 */
-	function spawnSquare(vertices, relativeCoordinates)
+	function spawnO(vertices, relativeCoordinates)
 	{
-		var square = [
+		var O = [
 				vec2.fromValues(-1, -1), vec2.fromValues(-1, 1), vec2.fromValues(1, -1),
 				vec2.fromValues(-1, 1), vec2.fromValues(1, -1), vec2.fromValues(1, 1) ];	
 		
-		for( var i = 0; i < square.length; i++ )
+		for( var i = 0; i < O.length; i++ )
 			for( var j = 0; j < DIMENSIONS; j++ )
-				vertices.push(square[i][j]);
+				vertices.push(O[i][j]);
 		
 		// relative coordinates refer to the center of each block
 		// by this it will be easier to perform rotations by a simple multiplication with a rotation matrix
@@ -50,14 +23,14 @@ function Tetromino()
 		relativeCoordinates.push(vec2.fromValues(0.5, 0.5));
 	}
 
-	function spawnIBlock(vertices, relativeCoordinates)
+	function spawnI(vertices, relativeCoordinates)
 	{	
-		var iBlock = [
+		var I = [
 				vec2.fromValues(-2, 0), vec2.fromValues(2, 0), vec2.fromValues(-2, 1),
 				vec2.fromValues(2, 0), vec2.fromValues(-2, 1), vec2.fromValues(2, 1) ];
-		for( var i = 0; i < iBlock.length; i++ )
+		for( var i = 0; i < I.length; i++ )
 			for( var j = 0; j < DIMENSIONS; j++ )
-				vertices.push(Math.round(iBlock[i][j]));
+				vertices.push(I[i][j]);
 		
 		relativeCoordinates.push(vec2.fromValues(-1.5,0.5));
 		relativeCoordinates.push(vec2.fromValues(-0.5,0.5));
@@ -65,74 +38,126 @@ function Tetromino()
 		relativeCoordinates.push(vec2.fromValues(1.5,0.5));
 	}
 
-/*	function spawnPodium()
+	function spawnT(vertices, relativeCoordinates)
 	{	
-		var vertices = [];
-		
-		var podium = [
-				vec2.fromValues(-1.5,-1), vec2.fromValues(-1.5, 0), vec2.fromValues(1.5, -1),
-				vec2.fromValues(-1.5, 0), vec2.fromValues(1.5, -1), vec2.fromValues(1.5, 0),
-				vec2.fromValues(-0.5, 0), vec2.fromValues(-0.5, 1), vec2.fromValues(0.5, 0),
-				vec2.fromValues(-0.5, 1), vec2.fromValues(0.5, 0), vec2.fromValues(0.5, 1) ];	
+		var T = [
+				vec2.fromValues(-1,-1), vec2.fromValues(-1, 0), vec2.fromValues(2, -1),
+				vec2.fromValues(-1, 0), vec2.fromValues(2, -1), vec2.fromValues(2, 0),
+				vec2.fromValues(0, 0), vec2.fromValues(0, 1), vec2.fromValues(1, 0),
+				vec2.fromValues(0, 1), vec2.fromValues(1, 0), vec2.fromValues(1, 1) ];
 
-		for( var i = 0; i < podium.length; i++ )
+		for( var i = 0; i < T.length; i++ )
 			for( var j = 0; j < DIMENSIONS; j++ )
-				vertices.push(Math.round(podium[i][j]));
+				vertices.push(T[i][j]);
 
-		return vertices;
+		relativeCoordinates.push(vec2.fromValues(-0.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(0.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(1.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(0.5,0.5));	
 	}
 
-	function spawnZBlock()
+	function spawnS(vertices, relativeCoordinates)
 	{
-		var vertices = [];
-			
-		var zBlock = [
-				vec2.fromValues(-1.5, -1), vec2.fromValues(-1.5, 0), vec2.fromValues(0.5, -1),
-				vec2.fromValues(-1.5, 0), vec2.fromValues(0.5, -1), vec2.fromValues(0.5, 0),
-				vec2.fromValues(-0.5, 0), vec2.fromValues(-0.5, 1), vec2.fromValues(1.5, 0),
-				vec2.fromValues(-0.5, 1), vec2.fromValues(1.5, 0), vec2.fromValues(1.5, 1) ];
+		var S = [
+				vec2.fromValues(-1, -1), vec2.fromValues(-1, 0), vec2.fromValues(1, -1),
+				vec2.fromValues(-1, 0), vec2.fromValues(1, -1), vec2.fromValues(1, 0),
+				vec2.fromValues(0, 0), vec2.fromValues(0, 1), vec2.fromValues(2, 0),
+				vec2.fromValues(0, 1), vec2.fromValues(2, 0), vec2.fromValues(2, 1) ];
 		
-		for( var i = 0; i < zBlock.length; i++ )
+		for( var i = 0; i < S.length; i++ )
 			for( var j = 0; j < DIMENSIONS; j++ )
-				vertices.push(Math.round(zBlock[i][j]));
-				
-		return vertices;
+				vertices.push(S[i][j]);
+		
+		relativeCoordinates.push(vec2.fromValues(-0.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(0.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(0.5,0.5));
+		relativeCoordinates.push(vec2.fromValues(1.5,0.5));	
 	}
-
-	function spawnLBlock()
+	
+	function spawnZ(vertices, relativeCoordinates)
 	{
-		var vertices = [];
-
-		var lBlock = [
-				vec2.fromValues(-1, -1.5), vec2.fromValues(-1, 1.5), vec2.fromValues(0, -1.5),
-				vec2.fromValues(-1, 1.5), vec2.fromValues(0, -1.5), vec2.fromValues(0, 1.5),
-				vec2.fromValues(0, -1.5), vec2.fromValues(0, -0.5), vec2.fromValues(1, -1.5),
-				vec2.fromValues(0, -0.5), vec2.fromValues(1, -1.5), vec2.fromValues(1, -0.5) ];
-
-		for( var i = 0; i < lBlock.length; i++ )
-			for( var j = 0; j < DIMENSIONS; j++ )
-				vertices.push(Math.round(lBlock[i][j]));
+		var Z = [
+				vec2.fromValues(0, -1), vec2.fromValues(0, 0), vec2.fromValues(2, -1),
+				vec2.fromValues(0, 0), vec2.fromValues(2, -1), vec2.fromValues(2, 0),
+				vec2.fromValues(-1, 0), vec2.fromValues(-1, 1), vec2.fromValues(1, 0),
+				vec2.fromValues(-1, 1), vec2.fromValues(1, 0), vec2.fromValues(1, 1) ];
 		
-		return vertices;
+		for( var i = 0; i < Z.length; i++ )
+			for( var j = 0; j < DIMENSIONS; j++ )
+				vertices.push(Z[i][j]);
+		
+		relativeCoordinates.push(vec2.fromValues(0.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(1.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(-0.5,0.5));
+		relativeCoordinates.push(vec2.fromValues(0.5,0.5));	
 	}
-*/
+
+
+	function spawnL(vertices, relativeCoordinates)
+	{
+		var L = [
+				vec2.fromValues(-1, -1), vec2.fromValues(-1, 1), vec2.fromValues(0, -1),
+				vec2.fromValues(-1, 1), vec2.fromValues(0, -1), vec2.fromValues(0, 1),
+				vec2.fromValues(0, 0), vec2.fromValues(0, 1), vec2.fromValues(2, 0),
+				vec2.fromValues(0, 1), vec2.fromValues(2, 0), vec2.fromValues(2, 1) ];
+
+		for( var i = 0; i < L.length; i++ )
+			for( var j = 0; j < DIMENSIONS; j++ )
+				vertices.push(L[i][j]);
+
+		relativeCoordinates.push(vec2.fromValues(-0.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(-0.5,0.5));
+		relativeCoordinates.push(vec2.fromValues(0.5,0.5));
+		relativeCoordinates.push(vec2.fromValues(1.5,0.5));	
+	}
+
+	function spawnJ(vertices, relativeCoordinates)
+	{
+		var J = [
+				vec2.fromValues(0, -1), vec2.fromValues(0, 1), vec2.fromValues(1, -1),
+				vec2.fromValues(0, 1), vec2.fromValues(1, -1), vec2.fromValues(1, 1),
+				vec2.fromValues(-2, 0), vec2.fromValues(-2, 1), vec2.fromValues(0, 0),
+				vec2.fromValues(-2, 1), vec2.fromValues(0, 0), vec2.fromValues(0, 1) ];
+
+		for( var i = 0; i < J.length; i++ )
+			for( var j = 0; j < DIMENSIONS; j++ )
+				vertices.push(J[i][j]);
+
+		relativeCoordinates.push(vec2.fromValues(0.5,-0.5));
+		relativeCoordinates.push(vec2.fromValues(-1.5,0.5));
+		relativeCoordinates.push(vec2.fromValues(-0.5,0.5));
+		relativeCoordinates.push(vec2.fromValues(0.5,0.5));	
+	}
+
 	function spawnRandom(vertices, relativeCoordinates)
 	{
-//		var random = Math.floor(Math.random() * 5);
-		var random = Math.floor(Math.random() * 2);
+		var random = Math.floor(Math.random() * 7);
 		
 		switch(random)
 		{
-			case 0: return spawnSquare(vertices, relativeCoordinates);
-			case 1:	return spawnIBlock(vertices, relativeCoordinates);
-//			case 2: return spawnPodium();
-//			case 3: return spawnLBlock();
-//			case 4: return spawnZBlock();
+			case 0: spawnO(vertices, relativeCoordinates);
+					break;
+			case 1:	spawnI(vertices, relativeCoordinates);
+					break;
+			case 2: spawnT(vertices, relativeCoordinates);
+					break;
+			case 3: spawnS(vertices, relativeCoordinates);
+					break;
+			case 4: spawnZ(vertices, relativeCoordinates);
+					break;
+			case 5: spawnL(vertices, relativeCoordinates);
+					break;
+			case 6: spawnJ(vertices, relativeCoordinates);
+					break;
 		}
 	}
 	
 	function addColor(vertices)
 	{
+		// i simply want to have onecolored tetrominos
+		// -> assign each vertex the same color value
+		// for some fancy rainbowcolor effects assign different colors to the vertices but I was told rainbowcolor is bad ;)
+		// should not be to bright or dark -> use only random values between 50 and 200
 		var col = [];
 		var col1 = (Math.floor(Math.random()*150)+50)/256;
 		var col2 = (Math.floor(Math.random()*150)+50)/256;
@@ -141,9 +166,7 @@ function Tetromino()
 		for (var i=0; i < vertices.length/DIMENSIONS; i++)
 			col = col.concat( [col1, col2, col3, 1.0 ] );
 			
-		return col;
-			
-		
+		return col;	
 	}
 	
 	this.vertices = [];
@@ -151,13 +174,7 @@ function Tetromino()
 	this.modelViewMatrix = [];
 
 	spawnRandom(this.vertices, this.relativeCoordinates);
-	this.color = addColor(this.vertices);
-	
-	// i simply want to have onecolored tetrominos
-	// -> assign each vertex the same color value
-	// for some fancy rainbowcolor effects assign different colors to the vertices but I was told rainbowcolor is bad ;)
-	// should not be to bright or dark -> use only random values between 50 and 200
-	
+	this.color = addColor(this.vertices);	
 }
 
 
